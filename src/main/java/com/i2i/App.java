18c735f634f3af974ce5d2185023e70209c1d864
MustafaOguzDuman <oguz.duman@ug.bilkent.edu.tr>
2022-08-11 19:15:55 +0300

    random slect 1 in 5

--- src/main/java/com/i2i/App.java
+++ src/main/java/com/i2i/App.java
@@ -3,19 +3,23 @@ package com.i2i;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.IOException;
+import java.util.Random;
 
 public class App 
 {
     public static final Logger logger = LogManager.getLogger(App.class);
-    public static void main( String[] args ) throws IOException {
+    public static void main( String[] args ) {
         //BasicConfigurator.configure();
 
         MSISDNGenerator msisdnGenerator = new MSISDNGenerator();
         LocationGenerator locationGenerator = new LocationGenerator();
         ServiceGenerator serviceGenerator = new ServiceGenerator();
         AmountGenerator amountGenerator = new AmountGenerator();
+        Random rand = new Random();
         while(true) {
+            if(rand.nextInt()%5!=0){ //randomly select 1 of 5 cycles
+                continue;
+            }
             String msisdn = msisdnGenerator.getMsisdn();
             String location = locationGenerator.getLocation();
             String service = serviceGenerator.getService();
